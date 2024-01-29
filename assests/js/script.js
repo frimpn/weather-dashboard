9060ca62d3ccd98cc6bee4c74b86c654944b0411
Nana <frimpongnana8@gmail.com>
2024-01-29 22:27:01 +0000

    added the api url and the fetch

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,4 @@
-//let weatherSearch = $('#search-input').val()
+
 let btn = $('#search-button')
 
 
@@ -8,4 +8,24 @@ function searchCity(event){
     event.preventDefault()
     let weatherSearch = $('#search-input').val()
 console.log(weatherSearch)
+let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${weatherSearch}&appid=7c8d3234ce864a983dd34685b2d8e1da`
+
+fetch(apiUrl)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(data){
+    console.log(data)
+    let temp =  data.main.temp
+    let celisus = Math.floor(temp -  273.15)
+    console.log(temp)
+    
+
+    let humidity = data.main.humidity
+    console.log(humidity)
+    let wind = data.wind.speed
+    console.log(wind)
+
+  })
+
 }
