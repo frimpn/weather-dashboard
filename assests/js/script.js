b205c004c2d47e8216e39e28adf4cc70415d886f
Nana <frimpongnana8@gmail.com>
2024-01-30 02:01:03 +0000

    added the next five days forecast

--- assests/js/script.js
+++ assests/js/script.js
@@ -4,6 +4,7 @@ let btn = $('#search-button')
 
 btn.on('click', searchCity)
 
+
 function searchCity(event){
     event.preventDefault()
     let weatherSearch = $('#search-input').val()
@@ -15,12 +16,19 @@ fetch(apiUrl)
     return response.json()
   })
   .then(function(data){
-    console.log(data.name)
+    console.log(data)
     let temp =  data.main.temp
     let celisus = Math.floor(temp -  273.15)
     let humidity = data.main.humidity
      let wind = data.wind.speed
   let name = data.name
+  let lat = data.coord.lat
+  let lon = data.coord.lon
+
+  console.log(lon)
+
+
+
   let Cityname = $('<h1>').text(`${name} (${dayjs().format('DD/MM/YYYY')})`)
   let cityTemp = $('<p>').text(`Temp: ${celisus}°C `)
   let cityWind = $('<p>').text(`Wind: ${wind} KPH`)
@@ -33,11 +41,55 @@ fetch(apiUrl)
 
   
 
-
+nextDays(lat,lon)
   })
-
-
 
 
 }
 
+
+ function nextDays(lat,lon){
+
+  let apiUrl= `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7c8d3234ce864a983dd34685b2d8e1da`
+  console.log(apiUrl)
+
+ fetch(apiUrl)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(data){
+    console.log(data)
+    
+    let days = []
+   let differentDays = data.list.filter(forecast =>{
+      const forecastDate = new Date(forecast.dt_txt).getDate()
+      if(!days.includes(forecastDate))
+      return days.push(forecastDate)
+
+    })
+    console.log(differentDays)
+
+    for(let i = 1 ; i < differentDays.length; i++){
+      
+      let panels = $('<div>')
+
+    let date = $('<h3>')
+    date.text(differentDays[i].dt_txt)
+    
+    let temp = $('<p>')
+    temp.text(Math.floor(differentDays[i].main.temp - 273.15)+'°C')
+    let hum = $('<p>')
+    hum.text(differentDays[i].main.humidity+'%')
+
+    panels.append(date)
+    panels.append(temp)
+    panels.append(hum)
+
+
+    $('#forecast').append(panels)
+  
+    }
+    
+  })
+
+}
